2d99ed0b3c4e9423a19c0d3bfbf36fdaf68d899c
Dawit <dawitdiy@gmail.com>
2025-07-13 19:34:36 +0300

    react assignemnt done

---
+++ alx-fe-reactjs/alx-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
