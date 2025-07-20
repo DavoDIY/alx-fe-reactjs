c39f092a14cdb21ad6872f184445a927d0a333b9
Dawit <dawitdiy@gmail.com>
2025-07-20 21:42:42 +0300

    import error

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ProfilePage from './ProfilePage';
 import { UserContext } from './UserContext'; 
-
+import { Counter } from './components/Counter';
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
